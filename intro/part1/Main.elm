33021f1f2f5261fc9c135ea07913fa2654dd08f6
Daisho Komiyama <daishokomiyama@gmail.com>
2022-04-17 21:19:42 -0400

    Add List exercise

--- intro/part1/Main.elm
+++ intro/part1/Main.elm
@@ -4,36 +4,44 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 
 
+pluralize singular plural quantity =
+    let
+        quantityString =
+            String.fromInt quantity
+
+        prefix =
+            quantityString ++ " "
+    in
+    if quantity == 1 then
+        prefix ++ singular
+
+    else
+        prefix ++ plural
+
+
+answer =
+    List.map (\str -> String.toUpper str ++ "!") [ "zap", "nip", "mop" ]
+
+
+banner : Html msg
 banner =
-    {- 👉 TODO: Add a logo and tagline to this banner, so its structure becomes:
-
-          <div class="banner">
-              <div class="container">
-
-                  <h1 class="logo-font">conduit</h1>
-
-                  <p>A place to share your knowledge.</p>
-
-               </div>
-          </div>
-
-       💡 HINT 1: the <div class="container"> above is an element with 2 child nodes.
-
-       💡 HINT 2: the <div class="feed-toggle"> below is an element with text.
-    -}
     div [ class "banner" ]
         [ div [ class "container" ]
-            [ text "👉 TODO: Put the <h1> here instead of this text, then add the <p> right after the <h1>" ]
+            [ h1 [ class "logo-font" ] [ text "conduit" ]
+            , p [] [ text "A place to share your knowledge." ]
+            ]
         ]
 
 
+feed : Html msg
 feed =
-    div [ class "feed-toggle" ] [ text "(In the future we’ll display a feed of articles here!)" ]
+    div [ class "feed-toggle" ] [ text "(In the future we'll display a feed of articles here!)" ]
 
 
+main : Html msg
 main =
     div [ class "home-page" ]
-        [ div [] [ text "👉 TODO: Replace this <div> with the banner" ]
+        [ banner
         , div [ class "container page" ]
             [ div [ class "row" ]
                 [ div [ class "col-md-9" ] [ feed ]
--- intro/part1/README.md
+++ intro/part1/README.md
@@ -17,3 +17,11 @@ Open `Main.elm` in your editor and resolve the TODOs there.
 
 After you complete each one, re-run `elm make Main.elm --output elm.js` to
 recompile the `elm.js` file, then refresh the browser to see the results!
+
+dyt*crv*jvt4vxg6NCT
+
+zqp@WRT@ehb9wdf9xcd
+
+ssh -i ~/.ssh/ssh-key-2021-12-15.key ubuntu@140.238.146.218
+
+192.226.167.238
